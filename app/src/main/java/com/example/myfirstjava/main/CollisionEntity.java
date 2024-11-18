c736c7588fa8fc5a19f49729cd1078eacf0647ff
WeirdOne999 <106362960+WeirdOne999@users.noreply.github.com>
2024-11-18 21:07:39 +0800

    AHHHHHHHHHHH

---
+++ app/src/main/java/com/example/myfirstjava/main/CollisionEntity.java
@@ -0,0 +1,28 @@
+package com.example.myfirstjava.main;
+
+import com.example.myfirstjava.mgp2d.core.GameEntity;
+import com.example.myfirstjava.mgp2d.core.Vector2;
+
+public class CollisionEntity extends GameEntity {
+    public int _layer = 0;
+    public void setLayer(int layer) {_layer = layer;}
+
+    protected Vector2 _size = new Vector2(0, 0);
+    public Vector2 getSize() { return _size.copy(); }
+    public void setSize(Vector2 size) { _size = size; }
+
+
+    private boolean isColliding(CollisionEntity collider) {
+        float Left = getPosition().x - getSize().x / 2;
+        float Right = getPosition().x + getSize().x / 2;
+        float Top = getPosition().y - (getSize().y / 2);
+        float Btm = getPosition().y + getSize().y / 2;
+
+        float cLeft = collider.getPosition().x - collider.getSize().x / 2;
+        float cRight = collider.getPosition().x + collider.getSize().x / 2;
+        float cTop = collider.getPosition().y - collider.getSize().y / 2;
+        float cBtm = collider.getPosition().y + collider.getSize().y / 2;
+
+        return Left < cRight && cLeft < Right && Top < cBtm && cTop < cBtm;
+    }
+}
