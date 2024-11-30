1d4c1bcf6d4b5b20d4e1734a0f76bb31f61467c0
Courtney06 <134606827+Courtney06@users.noreply.github.com>
2024-11-30 23:03:41 +0800

    aqilpush

--- app/src/main/java/com/example/myfirstjava/main/PlayerEntity.java
+++ app/src/main/java/com/example/myfirstjava/main/PlayerEntity.java
@@ -2,6 +2,7 @@ package com.example.myfirstjava.main;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -12,11 +13,27 @@ import com.example.myfirstjava.mgp2d.core.GameEntity;
 import com.example.myfirstjava.mgp2d.core.GameScene;
 import com.example.myfirstjava.mgp2d.core.Vector2;
 
+class ButtonStructure{
+    public ButtonStructure(int id, GameEntity entity){
+        this.id = id;
+        this.entity = entity;
+    }
+
+    public int id;
+    public GameEntity entity;
+}
+
 public class PlayerEntity extends GameEntity {
 
     //private boolean _isHolding = false;
     private int currentID = -1;
-    public int[] cursordrawable = new int[4];
+    private boolean render = false;
+    public ButtonStructure[] cursordrawable = new ButtonStructure[4];
+
+    public GameEntity _entityCache;
+
+    public int currentEntity = -1;
+
     public PlayerEntity(){
         /*
         Bitmap bmp = BitmapFactory.decodeResource(GameActivity.instance.getResources(), R.drawable.pause);
@@ -29,10 +46,10 @@ public class PlayerEntity extends GameEntity {
 
         setAnimatedSprite(Bitmap.createScaledBitmap(bmp,100 * 10,100* 10,true),bmp.getWidth() / 32,bmp.getWidth() / 32,12,0,0);
         setLayer(-1);
-        cursordrawable[0] = R.drawable.minibunny;
-        cursordrawable[1] = R.drawable.miniboar;
-        cursordrawable[2] = R.drawable.minibear;
-        cursordrawable[3] = R.drawable.miniwolf;
+        cursordrawable[0] = new ButtonStructure(R.drawable.minibunny,new Chicken(new Vector2(0,0),-1));
+        cursordrawable[1] = new ButtonStructure(R.drawable.miniwolf,new Llama(new Vector2(0,0),-1));
+        cursordrawable[2] = new ButtonStructure(R.drawable.minibear,new Llama(new Vector2(0,0),-1));
+        cursordrawable[3] = new ButtonStructure(R.drawable.miniboar,new Llama(new Vector2(0,0),-1));
 
     }
 
@@ -62,7 +79,7 @@ public class PlayerEntity extends GameEntity {
         
          */
 
-        Bitmap bmp = BitmapFactory.decodeResource(GameActivity.instance.getResources(), cursordrawable[MainGameScene.instance.ChangeCursorSpritei]);
+        Bitmap bmp = BitmapFactory.decodeResource(GameActivity.instance.getResources(), cursordrawable[MainGameScene.instance.ChangeCursorSpritei].id);
 
         if (bmp == null) {
 
@@ -87,9 +104,32 @@ public class PlayerEntity extends GameEntity {
         if (currentID == -1 && (motionEvent.getAction() == MotionEvent.ACTION_POINTER_DOWN) ||
         motionEvent.getAction() == MotionEvent.ACTION_DOWN){
             currentID = pointerId;
+            render = true;
         } else if (currentID != -1 && (motionEvent.getAction() == MotionEvent.ACTION_POINTER_UP) ||
                 motionEvent.getAction() == MotionEvent.ACTION_UP){
             currentID = -1;
+            render = false;
+
+            //TODO: COLLISION
+            if (MainGameScene.instance.ChangeCursorSpritei >= 0){
+                _entityCache = cursordrawable[MainGameScene.instance.ChangeCursorSpritei].entity;
+                for (int i = 0; i < gamescene._gameEntities.size();i++) {
+                    if (gamescene._gameEntities.get(i) != this) {
+                        if (gamescene._gameEntities.get(i) instanceof Holder) {
+                            if (this.touching(gamescene._gameEntities.get(i))) {
+                                Holder temp = (Holder)gamescene._gameEntities.get(i);
+                                GameEntity pointerToEntity = _entityCache;
+                                gamescene._gameEntityCache.add(temp);
+                                pointerToEntity.setPosition(temp.getPosition());
+                                pointerToEntity.setLayer(temp.getLayer());
+
+                            }
+                        }
+                    }
+                }
+            }
+
+            MainGameScene.instance.ChangeCursorSpritei = -1;
         }
         if (currentID != -1){
             for (int i = 0; i < motionEvent.getPointerCount(); i++){
@@ -101,4 +141,10 @@ public class PlayerEntity extends GameEntity {
 
         }
     }
+
+    @Override
+    public void onRender(Canvas canvas) {
+        if(render)super.onRender(canvas);
+    }
 }
+
